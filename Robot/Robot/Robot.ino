51363bb5e327ded76f7094f38ce1132d631e96b0
Jack Kalish <jykalish@gmail.com>
2018-03-17 18:03:46 -0400

    added arduino code for the robot

---
+++ Robot/Robot/Robot.ino
@@ -0,0 +1,32 @@
+#include <CapacitiveSensor.h>
+
+/*
+ * CapitiveSense Library Demo Sketch
+ * Paul Badger 2008
+ * Uses a high value resistor e.g. 10M between send pin and receive pin
+ * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
+ * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
+ */
+
+CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+
+void setup()                    
+{
+   Serial.begin(9600);
+}
+
+void loop()                    
+{
+    long start = millis();
+    long cap =  cs_4_8.capacitiveSensor(30);
+
+   // Serial.print(millis() - start);        // check on performance in milliseconds
+   // Serial.print("\t");                    // tab character for debug windown spacing
+
+  //  Serial.println(total);                  // print sensor output 1
+
+  if(cap > 0)
+    Serial.println("TOUCH!");
+   
+    delay(10);                             // arbitrary delay to limit data to serial port 
+}
